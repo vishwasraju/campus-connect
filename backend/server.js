242773b87668b791c81c42fc361204fb1deb0e05
Aditya-138-12 <adityasaroha456@gmail.com>
2026-01-24 10:08:06 +0530

    Vercel deployment failed for this architecture of mongoDB

--- backend/server.js
+++ backend/server.js
@@ -23,8 +23,8 @@ app.get("/", (req, res) => {
   res.json({ message: "Server is running."});
 });
 
-// app.listen(3000, (req, res) => {
-//   console.log("Server running on port 3000");
-// });
+app.listen(3000, (req, res) => {
+  console.log("Server running on port 3000");
+});
 
 export default app;
