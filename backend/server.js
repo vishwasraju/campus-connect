2127fffa6056eeb5b263d36d8ed2711c4cf8e13a
Aditya-138-12 <adityasaroha456@gmail.com>
2026-01-30 04:36:34 +0530

    New listen change

--- backend/server.js
+++ backend/server.js
@@ -28,5 +28,5 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, (req, res) => {
-  console.log("Server running on port 3000");
+  console.log(`Server running on port ${PORT}`);
 });
