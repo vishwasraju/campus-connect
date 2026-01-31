39a3e7e9bd2670f514d7247be1309c2ba9e09755
Aditya-138-12 <adityasaroha456@gmail.com>
2026-01-31 05:38:00 +0530

    Added other changes

--- backend/server.js
+++ backend/server.js
@@ -8,13 +8,13 @@ const cors = require('cors');
 const app = express();
 app.use(express.json());
 
-// app.use(cors({
-//   origin: "http://localhost:8080"
-// }));
+app.use(cors({
+  origin: "http://localhost:8080"
+}));
 
-app.use(cors());
+// app.use(cors());
 
-//connectDB();
+connectDB();
 
 app.use("/auth", authRoutes);
 app.use("/api", routes);
